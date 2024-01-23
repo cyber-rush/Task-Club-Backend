b5387afa776374cf427e8d92d0454e85dcbbb5ad
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2024-01-23 14:23:31 +0530

    ready to deploy another time

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config()
 const cors = require('cors')
 const { mongoose } = require('mongoose')
 const app = express()
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 8000;
 
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL)
     .catch((err) => console.log('Database not conected', err))
 
 //middleware
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const cors = require('cors')
 const dotenv = require('dotenv').config()
 const { test, registerUser, loginUser, getProfile, logoutUser } = require('../controllers/authController')
 
 //middleware
-
-router.use(cors());
 
 router.get('/', test)
 router.post('/register', registerUser)
